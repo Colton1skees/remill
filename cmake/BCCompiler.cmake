3bf4e880ab84bf2b2ae7dfa48a8847101dc9d0a3
Colton1skees <Colton1skees@gmail.com>
2023-07-07 08:36:04 -0500

    Hardcode paths again

--- cmake/BCCompiler.cmake
+++ cmake/BCCompiler.cmake
@@ -49,8 +49,8 @@ endif()
 # To fix this in the short term I've hardcoded both the bitcode compiler and linker.
 # TODO: Fix.
 
-set(CLANG_PATH "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang.exe")
-set(LLVMLINK_PATH "C:/Users/colton/source/repos/llvm-project-15.0.7.src/llvm/build/RelWithDebInfo/bin/llvm-link.exe")
+set(CMAKE_BC_COMPILER "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang.exe")
+set(CMAKE_BC_LINKER "C:/Users/colton/source/repos/llvm-project-15.0.7.src/llvm/build/RelWithDebInfo/bin/llvm-link.exe")
 message(STATUS "The following compiler has been selected to compile the bitcode: ${CMAKE_BC_COMPILER}")
 message(STATUS "The following linker has been selected to link the bitcode: ${CMAKE_BC_LINKER}")
 
